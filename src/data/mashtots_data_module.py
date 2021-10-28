8a2cc3b1c27316dc9deee941ca9581d882b26c48
Ea0011 <avagyanee@gmail.com>
2021-10-29 01:33:47 +0400

    init: initialize project and boilerplate

---
+++ src/data/mashtots_data_module.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from torch.utils.data import DataLoader
+
+class MashtotsDataModule(pl.LightningDataModule):
+  def __init__(self, train_set, val_set, test_set, batch_size: 32, **kwargs) -> None:
+    super().__init__()
+    self.train_data = train_set
+    self.val_data = val_set
+    self.test_data = test_set
+    self.batch_size = batch_size
+
+  def setup(self):
+    self.training_set = self.train_data
+    self.validation_set = self.val_data
+    self.test_set = self.test_data
+
+  def train_dataloader(self):
+    return DataLoader(self.training_set, batch_size=self.batch_size, shuffle=True, num_workers=4)
+
+  def val_dataloader(self):
+    return DataLoader(self.validation_set, batch_size=self.batch_size, shuffle=True)
+
+  def test_dataloader(self):
+    return DataLoader(self.test_set, batch_size=self.batch_size, shuffle=True)
---
+++ src/networks/recognition_network.py
@@ -0,0 +1,99 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets, transforms
+import torchvision
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from torchvision import utils
+import numpy as np
+
+class HandwrittingRecognitionNet(pl.LightningModule):
+  def __init__(self, hparams):
+    """
+    Initialize the model from a given dict containing all your hparams
+    """
+    super(HandwrittingRecognitionNet, self).__init__()
+    for key in hparams.keys():
+        self.hparams[key]=hparams[key]
+
+    self.model = self.build_model(hparams)
+    self.loss_func = torch.nn.CrossEntropyLoss(reduction='mean')
+
+    def init_weights(layer):
+      if type(layer) in [nn.Conv2d]:
+          nn.init.kaiming_uniform_(layer.weight, nonlinearity="relu")
+          nn.init.constant_(layer.bias, 0.01)
+
+      # initialize layer weights
+      self.model.apply(init_weights)
+
+  def build_model(self, hparams):
+    model = nn.Sequential(
+      nn.Conv2d(3, 512, 1),
+      nn.BatchNorm2d(512),
+      nn.ReLU(512),
+      nn.Dropout(0.2),
+      nn.Conv2d(512, 256, 3, padding=1),
+      nn.BatchNorm2d(256),
+      nn.ReLU(256),
+      nn.Dropout(0.2),
+      nn.Conv2d(256, 128, 3, padding=1),
+      nn.BatchNorm2d(128),
+      nn.ReLU(128),
+    )
+
+    return model
+
+  def forward(self, x):
+    letter = self.model(x)
+
+    return letter
+
+  def general_end(self, loss, mode):
+    avg_loss = torch.stack([x for x in loss]).mean()
+    return avg_loss
+
+  def general_step(self, batch, batch_idx, mode="train"):
+    inputs = batch
+    images, labels = inputs["image"], inputs["letter"]
+    
+    predicted_points = self.forward(images)
+
+    loss = self.loss_func(predicted_points.view_as(labels), labels)
+    return loss
+
+  def training_step(self, batch, batch_idx):
+    loss = self.general_step(batch, batch_idx)
+    self.log('train_loss', loss)
+
+    return loss
+  
+  def validation_step(self, batch, batch_idx):
+    loss = self.general_step(batch, batch_idx, mode="val")
+    self.log('val_loss', loss)
+
+    return loss
+  
+  def validation_epoch_end(self, outputs):
+    avg_loss = self.general_end(outputs, "val")
+    print("Avg-Loss={}".format(avg_loss))
+    tensorboard_logs = {'val/loss': avg_loss}
+
+    return {'val_loss': avg_loss, 'log': tensorboard_logs}
+
+  def configure_optimizers(self):
+    optimizer = None
+
+    if self.hparams["optimizer"] == "SGD":
+        optimizer = torch.optim.SGD(self.model.parameters(), lr=self.hparams["learning_rate"], weight_decay = self.hparams["weight_decay"])
+    if self.hparams["optimizer"] == "Adam":
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.hparams["learning_rate"], weight_decay = self.hparams["weight_decay"])
+    if self.hparams["optimizer"] == "AdamW":
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.hparams["learning_rate"], weight_decay = self.hparams["weight_decay"])
+
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, min_lr=1e-8, verbose=True, factor=0.5)
+
+    return {"optimizer": optimizer, "scheduler": scheduler}
+
