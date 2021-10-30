badf9effda8f3f8ec3a5891c404bf1017c89f9fa
Ea0011 <avagyanee@gmail.com>
2021-10-31 03:01:27 +0400

    docs(module_builder): add small docs for module builder

--- src/networks/utils/module_builder.py
+++ src/networks/utils/module_builder.py
@@ -3,6 +3,12 @@ import torch
 import torch.nn as nn
 
 class ModuleBuilder():
+  """
+  Builds a Module List with given blocks and params for each block
+  Args
+    Block(nn.Module):                 A module to include in each layer
+    block_params(List[BlockParams]):  A list of params for each block   
+  """
   def __init__(self, Block, block_params: List[Dict]) -> None:
     self.Block = Block
     self.ModuleList = nn.ModuleList()
